a47041752766c8e823e4a6b69e639e0bcc30612f
Adam Sommer <adam@thehoick.com>
2018-01-02 16:35:29 -0500

    Adjusted Book ordering and added corrected updated_at field name.

--- books/models.py
+++ books/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    udpated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     about = models.TextField(max_length=1024)
     isbn = models.CharField(max_length=100)
     file_url = models.URLField(blank=True, default="")
     current_loc = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-updated_at',]
 
     def __str__(self):
         return self.title
