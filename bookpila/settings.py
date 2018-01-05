c2ebf036b3342e4416403d3ea1cab2fd912eb746
Adam Sommer <adam@thehoick.com>
2018-01-05 15:55:43 -0500

    Added Throttling to the API.

--- bookpila/settings.py
+++ bookpila/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'books',
 ]
 
@@ -130,11 +131,19 @@ MEDIA_URL = '/media/'
 # REST rest_framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICAION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 8,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '50/minute'
+    }
 }
