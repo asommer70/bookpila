74da6b2be2b5e4044df8afb30ea3539938f702de
Adam Sommer <adam@thehoick.com>
2018-01-06 08:03:22 -0500

    Migrated to PostgreSQL.

--- bookpila/settings.py
+++ bookpila/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'bookpila.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookpila_dev',
+        'USER': 'adam',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
