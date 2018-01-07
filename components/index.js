f8322ae7a1dd35e19c6703a84267c6a66b16ac81
Adam Sommer <adam@thehoick.com>
2018-01-07 08:46:07 -0500

    Changed order of cover image to use the uploaded image first instead of the generated image from the book file. And working on using Futurepress to read ePub files in browser.

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const https = require('https');
+
 const assets =  '../assets/js/vendor/';
 
 if (!fs.existsSync(assets)){
@@ -9,3 +11,21 @@ fs.createReadStream('./node_modules/foundation-sites/dist/js/foundation.min.js')
   .pipe(fs.createWriteStream(assets + 'foundation.min.js'));
 fs.createReadStream('./node_modules/jquery/dist/jquery.min.js')
   .pipe(fs.createWriteStream(assets + 'jquery.min.js'));
+
+const epubMinUrl = 'https://raw.githubusercontent.com/futurepress/epub.js/master/build/epub.min.js';
+const epubMinFile = fs.createWriteStream(assets + 'epub.min.js');
+const epubMinReq = https.get(epubMinUrl, function(response) {
+  response.pipe(epubMinFile);
+});
+
+const epubUrl = 'https://raw.githubusercontent.com/futurepress/epub.js/master/build/epub.js';
+const epubFile = fs.createWriteStream(assets + 'epub.js');
+const epubReq = https.get(epubUrl, function(response) {
+  response.pipe(epubFile);
+});
+
+const zipUrl = 'https://raw.githubusercontent.com/futurepress/epub.js/master/build/libs/zip.min.js';
+const zipFile = fs.createWriteStream(assets + 'zip.min.js');
+const zipReq = https.get(zipUrl, function(response) {
+  response.pipe(zipFile);
+});
